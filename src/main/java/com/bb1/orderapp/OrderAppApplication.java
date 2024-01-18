dc2fc208416369d5c382da1672550e94005068cf
ozyilmazm <M.Ozyilmaz@wilgenhaege.nl>
2024-01-18 21:50:41 +0100

    Initial Commit

---
+++ src/main/java/com/bb1/orderapp/OrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.bb1.orderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderAppApplication.class, args);
+    }
+
+}
